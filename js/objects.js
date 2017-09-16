4822ab4ac8df803692bac28f9f10638971985784
Thomas Davis <tdavis@outlook.com.au>
2017-09-17 01:46:31 +1000

    Init

---
+++ js/objects.js
@@ -0,0 +1,14 @@
+//template for building objects, e.g. drone, queen, worker, etc.
+
+function Person(name){
+  this.name = name
+  this.getName = function(){
+    return this.name
+  }
+}
+
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+
+});
