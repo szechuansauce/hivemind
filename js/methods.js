5ff90a4ce897c2924c254699070686c5d51373eb
Thomas Davis <tdavis@outlook.com.au>
2017-09-18 00:47:45 +1000

    basic objects and methods

--- js/methods.js
+++ js/methods.js
@@ -1,4 +1,47 @@
-// A $( document ).ready() block.
 $( document ).ready(function() {
+
+   //------------------------------------------utility functions
+   //update the counters
+   function updateBeeCounters() {
+      var $queenCount = $('#queenCount .count'),
+          $droneCount = $('#droneCount .count'),
+          $workerCount = $('#workerCount .count'),
+          $honeyCount = $('#honeyCount .count'),
+          $territoryCount = $('#territoryCount .count'),
+          $healthCount = $('#healthCount .count')
+      //methods
+      $queenCount.html();
+      $droneCount.html();
+      $workerCount.html();
+      $honeyCount.html($hive.honey);
+      $territoryCount.html($hive.territory);
+      $healthCount.html($hive.health);
+   }
+
+   //update the hive's finances - function for each type then called together
+   //multiplier = factor based upon number of workers currently influencing the type
+   function updateHoney(mult) {
+      $hive.honey += mult;
+   }
+   function updateTerritory(mult) {
+      $hive.territory += mult;
+   }
+   function updateHealth(mult) {
+      $hive.health += mult;
+   }
+   function updateAllFinances() {
+      updateHoney(100);
+      updateTerritory(10);
+      updateHealth(5);
+   }
+
+
+   //the update master method
+   setInterval(function() {
+      console.log('Updated.');
+      //update the counters
+      updateBeeCounters();
+      updateAllFinances();
+   }, 1000);
 
 });
--- js/objects.js
+++ js/objects.js
@@ -1,14 +1,72 @@
-//template for building objects, e.g. drone, queen, worker, etc.
-
-function Person(name){
-  this.name = name
-  this.getName = function(){
-    return this.name
-  }
+//Set up the Hive Prototype
+function hiveModel() {
+   //variables
+   this.name = 'Hive';
+   this.cost = 10000;
+   this.population = 0;
+   this.honey = 1000;
+   this.territory = 0;
+   this.health = 0;
+   this.queenCount = 0;
+   this.droneCount = 0;
+   this.workerCount = 0;
+   //methods
+   this.getHoney = function() {
+      console.log(this.honey);
+   };
 }
 
+//convert prototype to obj for use
+$hive = new hiveModel();
 
-// A $( document ).ready() block.
-$( document ).ready(function() {
+//Set up the Bee Prototype
+function beeModel() {
+   //variables
+   this.name = 'Bee';
+   this.cost = 0;
+   this.gender = 'Female';
+   this.canAddHoney = false;
+   this.canAddTerritory = false;
+   this.canAddHealth = false;
+   //methods
+}
 
-});
+// The Queen
+$queenBee = new beeModel();
+$queenBee.name = 'Queen';
+$queenBee.cost = 5000;
+
+// The Drone
+$droneBee = new beeModel();
+$droneBee.name = 'Drone';
+$droneBee.cost = 500;
+$droneBee.gender = 'Male';
+
+// The Basic Worker - alternates between each of the possible worker jobs - honey, territory, health.
+$workerBee = new beeModel();
+$workerBee.name = 'Worker';
+$workerBee.cost = 5;
+$workerBee.canAddHoney = true;
+$workerBee.canAddTerritory = true;
+$workerBee.canAddHealth = true;
+
+// The Honey Specialist Worker - Gathers Hive Honey Faster
+var $honeyWorkerBee = Object.create($workerBee);
+$honeyWorkerBee.name = 'Honey Worker';
+$honeyWorkerBee.cost = 50;
+$honeyWorkerBee.gender = 'Female';
+$honeyWorkerBee.canAddHoney = true;
+
+// The Maintenance Specialist Worker - Gathers Hive Territory Faster
+var $maintenanceWorkerBee = Object.create($workerBee);
+$maintenanceWorkerBee.name = 'Territory Worker';
+$maintenanceWorkerBee.cost = 50;
+$maintenanceWorkerBee.gender = 'Female';
+$maintenanceWorkerBee.canAddHealth = true;
+
+// The Territory Specialist Worker - Gathers Hive Health Faster
+var $territoryWorkerBee = Object.create($workerBee);
+$territoryWorkerBee.name = 'Territory Worker';
+$territoryWorkerBee.cost = 50;
+$territoryWorkerBee.gender = 'Female';
+$territoryWorkerBee.canAddTerritory = true;
